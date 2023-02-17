9508dba19ea19df01b840794c5d9823196636be9
Botsetse <beejaymamabolo@gmail.com>
2023-02-17 01:50:28 -0800

    bytes size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,21 +3,21 @@
 /**
  * main - Entry point
  *
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char sizeChar = 's';
 	int sizeInt = 100;
-	long int sizeLongint = 100;
-	long long int sizeLonglint = 100;
+	long int sli = 100;
+	long long int slli = 100;
 	float sizeFloat = 10.9f;
 
 	printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(sizeChar));
 	printf("\nSize of an int: %lu byte(s)", (unsigned long)sizeof(sizeInt));
-	printf("\nSize of a long int: %lu byte(s)", (unsigned long)sizeof(sizeLongint));
-	printf("\nSize of a long long int: %lu byte(s)", (unsigned long)sizeof(sizeLonglint));
+	printf("\nSize of a long int: %lu byte(s)", (unsigned long)sizeof(sli));
+	printf("\nSize of a long long int: %lu byte(s)", (unsigned long)sizeof(slli));
 	printf("\nSize of a float: %lu byte(s)\n", sizeof(sizeFloat));
 	return (0);
 }
